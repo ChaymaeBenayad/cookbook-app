6a5f03e7f2bd314c30aee72cb4cf9ff4fbf1ee4b
BENAYAD Chaymae <chaymae.benayad@etu.uae.ac.ma>
2022-10-14 01:00:42 +0100

    update config.js file

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
-export const API_KEY = "b1e491a3-30fd-42dd-a5d9-bab61719d6e6";
+export const API_KEY = ""; //You can get it from https://forkify-api.herokuapp.com/v2
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const MODAL_CLOSE_SEC = 2.5;
